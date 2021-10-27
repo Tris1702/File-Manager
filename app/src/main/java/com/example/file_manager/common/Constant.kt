4b90c060728cbacbc65d700c9c6bd73e4dc1a60b
Tris1702 <phuonghoand2001@gmail.com>
2021-10-27 11:25:01 +0700

    Feature open file in other app is done

--- app/src/main/java/com/example/file_manager/common/Constant.kt
+++ app/src/main/java/com/example/file_manager/common/Constant.kt
@@ -2,7 +2,14 @@ package com.example.file_manager.common
 
 object Constant {
     var musicEx = mutableListOf(".mp3", ".wav")
-    var videoEx = mutableListOf(".mp4", ".mvw")
-    var docEx = mutableListOf(".pdf")
+    var videoEx = mutableListOf(".mp4", ".avi", ".mpe")
+    var wordEx = mutableListOf(".doc", ".docx")
+    var plainTextEx = mutableListOf(".txt")
+    var pdfEx = mutableListOf(".pdf")
+    var pptEx = mutableListOf(".ppt", "pptx")
+    var excelEx = mutableListOf(".xls", ".xlsx")
+    var compressEx = mutableListOf(".zip", ".rar")
+    var imageEx = mutableListOf(".png", ".jpg",".jpeg")
+    var gifEx = mutableListOf(".gif")
     var path = ""
 }
--- app/src/main/java/com/example/file_manager/fragment/listAllFile/AllFileAdapter.kt
+++ app/src/main/java/com/example/file_manager/fragment/listAllFile/AllFileAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.file_manager.fragment.listAllFile
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.file_manager.R
 import com.example.file_manager.databinding.ItemFileBinding
 import java.io.File
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.example.file_manager.BuildConfig
+import com.example.file_manager.common.Constant
+import timber.log.Timber
+import java.util.Arrays
+
+
+
+
 
 class AllFileAdapter(private var onItemClick: (String) -> Unit)
     : RecyclerView.Adapter<AllFileAdapter.AllFileViewHolder>() {
@@ -25,9 +38,18 @@ class AllFileAdapter(private var onItemClick: (String) -> Unit)
                 else {
                     imgIconFile.setImageResource(R.drawable.ic_normal_file)
                 }
-            }
-            binding.root.setOnClickListener {
-                onItemClick(file.path)
+                root.setOnClickListener {
+                    if (file.isDirectory) onItemClick(file.path)
+                    else{
+                        openFile(file, binding.root.context)
+                    }
+                }
+                root.setOnLongClickListener {
+                    val filePath = file.absolutePath
+                    val intentShare = Intent(Intent.ACTION_VIEW)
+                    intentShare.type = "application/docx"
+                    true
+                }
             }
         }
 
@@ -48,5 +70,63 @@ class AllFileAdapter(private var onItemClick: (String) -> Unit)
             false
         )
         return AllFileViewHolder(binding)
+    }
+    private fun openFile(file: File, context: Context) {
+        val url = FileProvider.getUriForFile(
+            context,
+            BuildConfig.APPLICATION_ID+ ".provider",
+            file
+        )
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        if (checkIfFileHasExtension(url.toString(), Constant.wordEx)) {
+            Timber.d("Open word document")
+            intent.setDataAndType(url, "application/msword")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.pdfEx)) {
+            Timber.d("Open PDF file")
+            intent.setDataAndType(url, "application/pdf")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.pptEx)) {
+            Timber.d("Open Powerpoint file")
+            intent.setDataAndType(url, "application/vnd.ms-powerpoint")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.excelEx)) {
+            Timber.d("Open Excel file")
+            intent.setDataAndType(url, "application/vnd.ms-excel")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.compressEx)) {
+            Timber.d("Open zip file")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.musicEx)) {
+            Timber.d("Open audio file")
+            intent.setDataAndType(url, "audio/x-wav")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.imageEx)){
+            Timber.d("Open image file")
+            intent.setDataAndType(url, "image/*")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.plainTextEx)) {
+            Timber.d("Open plain text file")
+            intent.setDataAndType(url, "text/plain")
+        } else if (checkIfFileHasExtension(url.toString(), Constant.gifEx)){
+            Timber.d("Open gif file")
+            intent.setDataAndType(url, "image/gif")
+        }
+        else if (checkIfFileHasExtension(url.toString(), Constant.videoEx)) {
+            Timber.d("Open video file")
+            intent.setDataAndType(url, "video/*")
+        } else {
+            Timber.d("Open other files")
+            intent.setDataAndType(url, "*/*")
+        }
+        try {
+            if (file.exists()) context.startActivity(
+                Intent.createChooser(intent,"Open")
+            ) else Toast.makeText(context, "File is corrupted", Toast.LENGTH_LONG).show()
+        } catch (ex: Exception) {
+            Toast.makeText(context,"No Application is found to open this file. The File is saved at", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun checkIfFileHasExtension(s: String, extend: MutableList<String>): Boolean {
+        extend.forEach {
+            if (s.endsWith(it))
+                return true
+        }
+        return false
     }
 }
