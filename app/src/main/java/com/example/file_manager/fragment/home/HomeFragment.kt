55d2a53d10768c40f8036de590da61d0428bc4f1
tranhangt <tranhang13129@gmail.com>
2021-10-31 14:21:22 +0700

    home not done

---
+++ app/src/main/java/com/example/file_manager/fragment/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.file_manager.fragment.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.file_manager.R
+import com.example.file_manager.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imgOtherFile.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_fileListFragment)
+        }
+    }
+}
